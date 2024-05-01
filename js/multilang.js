7c404ef318f60ffa6a72f273e63c79195a3a9c1b
Shoky13 <alla-alla-luk@yandex.ru>
2024-05-01 18:31:21 +0300

    added multilingual interface

---
+++ js/multilang.js
@@ -0,0 +1,36 @@
+var arrLang = {
+    'ru': {
+      'title': 'Китайско-русский словарь',
+      'author': 'Автор Шоки',
+      'print': 'Напечатать',
+      'draw': 'Нарисовать',
+      'found_kanji': 'Найденные иероглифы',
+      'clear': 'Очистить',
+      'undo': 'Убрать последнюю черту',
+      'translate': 'Перевести',
+      'translated': 'Перевод',
+      'stroke_order': 'Порядок черт',
+    },
+    'zh': {
+        'title': '汉俄词典',
+        'author': '由瘦刻意',
+        'print': '印刷一下',
+        'draw': '画一下',
+        'found_kanji': '发现的汉字',
+        'clear': '清除一切',
+        'undo': '除去最后行程',
+        'translate': '翻译',
+        'translated': '翻译的',
+        'stroke_order': '笔顺',
+    }
+  }
+  
+    $(function() {
+      $('.translate').click(function() {
+        var lang = $(this).attr('id');
+  
+        $('.lang').each(function(index, item) {
+          $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+      });
+    });
